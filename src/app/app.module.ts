b479694e4ca4f6db2a765930ad79dcdef881be96
Matiluko Opeyemi Emmanuel <opmat01@yahoo.com>
2023-03-18 07:25:31 +0100

    chore: create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent 
+    AppComponent,
+    ProductItemComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemDetailComponent,
+    ProductListComponent 
   ],
   imports: [
     BrowserModule,
