3a411bd6649a79c087e856e104d9315758542af8
Matiluko Opeyemi Emmanuel <opmat01@yahoo.com>
2023-03-18 09:01:21 +0100

    chore: add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { CartComponent } from './components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'product/:id', component: ProductItemDetailComponent},
+  {path: 'confirmation', component: ConfirmationComponent},
+  {path: 'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
