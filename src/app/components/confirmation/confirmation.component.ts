00d2658dbe70ee30c3b6f1ae2d67c137de3cc38a
Matiluko Opeyemi Emmanuel <opmat01@yahoo.com>
2023-03-20 02:13:42 +0100

    feat: order confirmation component

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Buffer } from 'buffer'
 
 @Component({
   selector: 'app-confirmation',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent {
+  fullname: string = '';
+  totalPrice: number = 0;
 
+  constructor(
+    private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    let data: string = '';
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      const retdata:string = paramMap.get('info')!;
+      let data = JSON.parse(Buffer.from(retdata, 'base64').toString('binary'));
+      this.fullname = data.fullname;
+      this.totalPrice = data.totalPrice;
+    });
+  }
 }
